b631c3877cea25dd68fb727daa2cbd4f752ae978
mosby1596 <hoang159609@gmail.com>
2024-11-04 13:37:04 +0700

    Add project files.

---
+++ bai2_cong2so.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "chuong trinh tinh tong hai so" << endl;
+	int so1, so2; // khai bao bien so1 va so2 kieu so nguyen
+	int tong;
+	so1 = 3; // gan bien so1 va gia tri so3
+	so2 = 9;
+	tong = so1 + so2;
+	cout << "tong hai so:" << so1 << "+" << so2 << "=" << tong;
+	return 0;
+}
